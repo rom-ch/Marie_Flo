05ffdb01301c8964835ea7c6d65233c6e927cc64
Romain Chauvin <rom.chauvin@gmail.com>
2022-09-27 07:29:21 +0200

    form to spreadsheet

--- rsvp.js
+++ rsvp.js
@@ -52,3 +52,16 @@ checkboxFood.addEventListener('click', () => {
 		addFood.style.display = 'none';
 	}
 });
+
+// Submit Form
+
+const scriptURL =
+	'https://script.google.com/macros/s/AKfycbyQ8KJWFkbhBU3N71A4dF87CjNicIk8zjmOe2rdvSMphUdQoBTu9Ul3QmnSoLvNxSt0/exec';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+		.then(response => console.log('Success!', response))
+		.catch(error => console.error('Error!', error.message));
+});
