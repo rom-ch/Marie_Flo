a9dfbdf7276a66360990909ca568d81278bc7ecf
Romain Chauvin <rom.chauvin@gmail.com>
2022-10-21 13:27:16 +0200

    dj

--- js/dj.js
+++ js/dj.js
@@ -1,2 +1,24 @@
 const djSheet = document.querySelector('iframe');
 console.log(djSheet.contentWindow);
+
+// Submit Tracks
+
+const form = document.querySelector('.music');
+const submitButton = document.querySelector('.btn-music-submit');
+const scriptURL =
+	'https://script.google.com/macros/s/AKfycbzQRB4J4AIBOoJ5yFk58VFg4A-fgWOKSVBIf15tTOchr0CMQT9DTuoZJJ0VWwSZHqW5Lg/exec';
+
+form.addEventListener('submit', e => {
+	submitButton.disabled = true;
+	e.preventDefault();
+	let requestBody = new FormData(form);
+	fetch(scriptURL, { method: 'POST', body: requestBody })
+		.then(response => {
+			alert('Envoyé, Sent', response);
+			submitButton.disabled = false;
+		})
+		.catch(error => {
+			alert('Error!', error.message);
+			submitButton.disabled = false;
+		});
+});
