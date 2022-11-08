f19b9fa19119349c9d46f8a14596f80216db8896
Romain Chauvin <rom.chauvin@gmail.com>
2022-11-08 06:53:09 +0100

    lkj

--- js/contacts.js
+++ js/contacts.js
@@ -4,9 +4,17 @@ const flipCardInner = document.querySelectorAll('.flip-card-inner');
 
 flipCardInner.forEach(flip => {
 	flip.addEventListener('click', () => {
-		flip.classList.toggle('active');
+		flip.classList.add('active');
 	});
 });
+
+const removeActive = function () {
+	flipCardInner.forEach(flip => {
+		flip.classList.remove('active');
+	});
+};
+
+setTimeout(removeActive, 8000);
 
 const phoneNumbers = document.querySelectorAll('.contact-number');
 
@@ -15,16 +23,4 @@ phoneNumbers.forEach(phoneNumber => {
 		phoneNumber.attributes.href.value = '#';
 	}
 });
-
-// Copy Phone Numbers
-
-// function copyNumber() {
-// 	phoneNumbers.forEach(number => {
-// 		number.addEventListener('click', e => {
-// 			e.preventDefault();
-// 			navigator.clipboard.writeText(this.innerText).then(function () {
-// 				alert(`Number copied ${this.innerText}`);
-// 			});
-// 		});
-// 	});
-// }
+clearTimeout;
